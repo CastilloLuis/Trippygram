f51d2b5c490b845e0de510a14e296320e6dcb93b
CastilloLuis <luisedcastillog@gmail.com>
2018-07-03 20:30:55 -0400

    profile data logic

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import { EditProfilePage } from '../edit-profile/edit-profile';
+import { HttpProvider } from '../../providers/http/http';
 
 @IonicPage()
 @Component({
@@ -9,11 +10,25 @@ import { EditProfilePage } from '../edit-profile/edit-profile';
 })
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private modalCtrl: ModalController) {
+  profileData: Object = {};
+  post = [];
+  liked_posts = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private modalCtrl: ModalController, private http: HttpProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
+    this.http.fetch(null, 'GET', `profile.php?user_id=${1}`)
+      .subscribe((res) => {
+        console.log(res)
+        if(res.status === 200) {
+          console.log('asdasdasd')
+          this.profileData = res.data;
+          console.log(this.profileData)
+        }
+      });
+      console.log(this.profileData)
   }
 
   editProfile() {
